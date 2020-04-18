98d8a67c69d665d57e0f4538a36c1ce6f58662d4
B41052036 <63890559+B41052036@users.noreply.github.com>
2020-04-18 10:59:04 +0800

    Add files via upload

---
+++ Untitled.m
@@ -0,0 +1,9 @@
+%姓名：洪文宗
+%學號：B41052036
+clear;
+A=[123];
+B=[-1 3 -1];
+% dot
+c=dot(A,B)
+% cross
+D=cross(A,B)
---
+++ spower.m
@@ -0,0 +1,6 @@
+%姓名：洪文宗
+%學號：B41052036
+function p=spower(x)
+p=(norm(x)^2)/length(x);
+
+
